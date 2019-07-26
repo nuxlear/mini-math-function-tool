eade98ad1824a42195c7335512fb2d0127432565
nuxlear <nuxlear@estsoft.com>
2019-07-26 17:02:28 +0900

    feat: tokenizing function on lexer fix: wrong parenthesis in lexer_grammar

--- mathlib/io/lexer.py
+++ mathlib/io/lexer.py
@@ -1,4 +1,6 @@
+import itertools
 from collections import OrderedDict
+import re
 
 
 class Lexer:
@@ -25,14 +27,28 @@ class Lexer:
                 left, right = item
                 self.tokens[left] = right
 
-    @staticmethod
-    def tokenize(string):
-        pass
+    def tokenize(self, string):
+
+        def get_pattern(tokens):
+            chars = [x for x in tokens if len(x) == 1]
+            patterns = [x for x in tokens if x not in chars]
+
+            patterns.append('[{}]'.format(''.join(chars)).replace('-', '\\-'))
+
+            return '({})'.format('|'.join(map(lambda x: '({})'.format(x), patterns)))
+
+        pattern = get_pattern(self.tokens.values())
+        regex = re.compile(pattern)
+        tokens = [x[0] for x in regex.findall(string)]
+        return tokens
 
     def get_tokens(self):
         return tuple(self.tokens.keys())
 
 
 if __name__ == '__main__':
-    s = Lexer('lexer_grammar').tokens
-    print(s)
+    l = Lexer('lexer_grammar')
+    s = l.tokens
+    # print(s)
+    t = l.tokenize('3sinx + 4cosx * 5^2')
+    print(t)
