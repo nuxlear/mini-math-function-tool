5ccc2df9120b9a6717318de1b71d395188097062
nuxlear <nuxlear@estsoft.com>
2019-08-07 18:33:07 +0900

    refactor: delete old MathNode codes test: simplify log notation docs: add README.md chore: add .gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mini Math Function Tool
--- unittest/additional_test.py
+++ unittest/additional_test.py
@@ -13,9 +13,9 @@ class NotationTest(unittest.TestCase):
     def test_3(self):
         notation_test('1/(1/(1/(3+x)))')
 
-    # def test_4(self):
-    #     notation_test('log2_((x-1)^2) - tan((x-1)^2)')
-    #
+    def test_4(self):
+        notation_test('log2_(2^(x+1))')
+
     # def test_5(self):
     #     notation_test('(x-y+z)^0.2')
     #
